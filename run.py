82b6809196123de31db84bf7e9ec5ee76496bbb6
Daniel Schoenberger <schoenberger.daniel@outlook.com>
2024-07-23 14:59:59 +0200

    V0.2 first multi zones

--- run.py
+++ run.py
@@ -13,12 +13,16 @@ number_of_chains = 8
 m, n, ion_chain_size_vertical, ion_chain_size_horizontal = 3, 3, 2, 2
 pz1 = ProcessingZone('pz1', ((0, 0), (1, 0)))
 pz2 = ProcessingZone('pz2', ((3, 4), (4, 4)))
+pz3 = ProcessingZone('pz3', ((2, 2), (2, 3)))
 
 sequence = [1, 0, 4, 7, 6, 2, 3, 5]
-partition = {'pz1': [0, 1, 2], 'pz2': [3, 4, 5, 6, 7, 8, 9]}
+partition = {'pz1': [0, 1, 7], 'pz2': [3, 4, 9, 6], 'pz3': [2, 8, 5]}
 
-# make sure all elements are in one of the partition
-assert all(element in partition['pz1'] + partition['pz2'] for element in sequence)
+# Ensure all elements are in one of the partitions
+all_partition_elements = []
+for elements in partition.values():
+    all_partition_elements.extend(elements)
+assert all(element in all_partition_elements for element in sequence)
 # and no element is in both partitions
 pz_sets = {pz: set(elements) for pz, elements in partition.items()}
 common_elements = set.intersection(*pz_sets.values())
@@ -30,7 +34,7 @@ map_to_pz = {element: pz for pz, elements in partition.items() for element in el
 graph_creator = GraphCreator(m, n, ion_chain_size_vertical, ion_chain_size_horizontal)
 G = graph_creator.get_graph()
 G.plot = plot
-G.pzs = [pz1, pz2]
+G.pzs = [pz1, pz2, pz3]
 G.map_to_pz = map_to_pz
 
 create_starting_config(G, number_of_chains, seed=0)
--- shuttle.py
+++ shuttle.py
@@ -2,29 +2,6 @@ from Cycles import get_ion_chains
 from scheduling import create_move_list, create_priority_queue, get_partitioned_priority_queues, find_movable_cycles, rotate_free_cycles, create_cycles_for_moves, preprocess
 from plotting import plot_state
 
-# def shuttle(graph, sequence, partition, timestep):
-#     priority_queue = create_priority_queue(graph, sequence)
-#     preprocess(graph, priority_queue)
-
-#     # update ion chains after preprocesss
-#     graph.state = get_ion_chains(graph)
-
-#     part_prio_queues = get_partitioned_priority_queues(graph, priority_queue, partition)
-#     prio_queue_1 = part_prio_queues[graph.pzs[0].name]
-#     prio_queue_2 = part_prio_queues[graph.pzs[1].name]
-#     move_list_1 = create_move_list(graph, prio_queue_1, graph.pzs[0])
-#     move_list_2 = create_move_list(graph, prio_queue_2, graph.pzs[1])
-
-#     all_cycles_1 = create_cycles_for_moves(graph, move_list_1, graph.pzs[0])
-#     all_cycles_2 = create_cycles_for_moves(graph, move_list_2, graph.pzs[1])
-#     all_cycles = {**all_cycles_1, **all_cycles_2}
-
-#     chains_to_rotate = find_movable_cycles(graph, all_cycles, priority_queue)
-#     rotate_free_cycles(graph, all_cycles, chains_to_rotate)
-
-#     labels = ("timestep %s" % timestep, None)
-#     plot_state(graph, labels, plot_ions=True, show_plot=graph.plot, save_plot=False, plot_cycle=False, plot_pzs=True, filename="")
-
 def shuttle(graph, sequence, partition, timestep):
     priority_queue = create_priority_queue(graph, sequence)
     preprocess(graph, priority_queue)
